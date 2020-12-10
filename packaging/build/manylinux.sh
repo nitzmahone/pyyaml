e32ac17fee06649a33386d4eb179a29f51b7a47b
Matt Davis <mrd@redhat.com>
2020-12-10 14:53:06 -0800

    test cached libyaml

--- packaging/build/manylinux.sh
+++ packaging/build/manylinux.sh
@@ -2,30 +2,60 @@
 
 set -eux
 
-# We need to build this here rather than as a CI step
-# so that it's easily available in the container itself
-./packaging/build/libyaml.sh
-
-mkdir -p wheelhouse
-rm -vf wheelhouse/*.whl
+# FIXME: externalize the libyaml build for each container/arch as an ephemeral artifact
+#./packaging/build/libyaml.sh
 
 # PyYAML supports Python 2.7, 3.6-3.8
-for tag in $(echo $PYTHON_TAGS | tr ":" " "); do
-  PYBIN="/opt/python/${tag}/bin"
-  "${PYBIN}/python" -m pip install -U wheel setuptools pip Cython auditwheel
-  "${PYBIN}/python" -m pip wheel \
-    --verbose \
-    --no-deps \
-    --global-option '--with-libyaml' \
-    --global-option "build_ext" \
-    -w wheelhouse .
-done
+#for tag in $(echo $PYTHON_TAGS | tr ":" " "); do
+#  PYBIN="/opt/python/${tag}/bin"
+#  "${PYBIN}/python" -m pip install setuptools build==0.1.0 Cython auditwheel
+#  "${PYBIN}/python" -m build \
+#    --verbose \
+#    --no-deps \
+#    --global-option '--with-libyaml' \
+#    --global-option "build_ext" \
+#    -w wheelhouse .
+#done
 
-for whl in wheelhouse/*.whl; do
-  auditwheel repair "$whl" --plat "$PLAT" -w wheelhouse/
-  rm -f "$whl"
-done
+PYBIN="/opt/python/${PYTHON_TAG}/bin"
+PYYAML_FORCE_CYTHON=1
+PYYAML_FORCE_LIBYAML=1
 
-mkdir dist
-mv wheelhouse/*.whl dist/
-ls -1 dist/
+# we're using a private build of libyaml, so set paths to favor that instead of whatever's laying around
+C_INCLUDE_PATH=../libyaml/include:${C_INCLUDE_PATH:-}
+LIBRARY_PATH=../libyaml/src/.libs:${LIBRARY_PATH:-}
+LD_LIBRARY_PATH=../libyaml/src/.libs:${LD_LIBRARY_PATH:-}
+
+# install deps
+echo "::group::installing build deps"
+"${PYBIN}/python" -m pip install build==0.1.0 Cython
+echo "::endgroup::"
+
+if [[ ${PYYAML_RUN_TESTS:-1} -eq 1 ]]; then
+  echo "::group::running test suite"
+  # FIXME: split tests out for easier direct execution w/o Makefile
+  # run full test suite
+  make testall PYTHON="${PYBIN}/python"
+  echo "::endgroup::"
+else
+  echo "skipping test suite..."
+fi
+
+if [[ ${PYYAML_BUILD_WHEELS:-0} -eq 1 ]]; then
+  echo "::group::building wheels"
+  # FIXME: clean this up to use `build` once we're doing a proper PEP517 build
+  "${PYBIN}/python" -m pip wheel -w tempwheel --no-build-isolation .
+  echo "::endgroup::"
+
+  echo "::group::validating wheels"
+  # FIXME: smoke-test individual wheels by name/platform (no wildcard)
+  for whl in tempwheel/*.whl; do
+    auditwheel repair --plat "${AW_PLAT}" "$whl" -w dist/
+  done
+
+  ls -1 dist/
+
+  echo "::endgroup::"
+else
+  echo "skipping wheel build..."
+fi
