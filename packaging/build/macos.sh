106ddb38ef7a57003c961c8affd953a81a8f95f9
Matt Davis <mrd@redhat.com>
2020-12-10 20:14:22 -0800

    mac fun

--- packaging/build/macos.sh
+++ packaging/build/macos.sh
@@ -2,11 +2,33 @@
 
 set -eux
 
-/usr/bin/python3 -V
-/usr/bin/python3 -m pip install -U --user cibuildwheel
-PYYAML_FORCE_CYTHON=1 PYYAML_FORCE_LIBYAML=1 CIBW_BEFORE_BUILD='pip install cython' /usr/bin/python3 -m cibuildwheel --platform macos .
+# doesn't really matter which Python we use, so long as it can run cibuildwheels, and we're consistent within the
+# build, since cibuildwheel is internally managing looping over all the Pythons for us.
+export PYBIN=/usr/bin/python3
+
+# FIXME: run tests
+
+${PYBIN} -V
+${PYBIN} -m pip install -U --user cibuildwheel
+# run cibuildwheel; we can skip CIBW_ENVIRONMENT since the Mac version will directly inherit the envvars we set to
+# force Cython and --with-libyaml. cibuildwheel will install Cython before each version is built. We expect that
+# the calling environment will set CIBW_SKIP or CIBW_BUILD to control which Pythons we build for. (eg, CIBW_SKIP='pp* cp27* cp35*')
+
+# we're using a private build of libyaml, so set paths to favor that instead of whatever's laying around
+export C_INCLUDE_PATH=$(cd libyaml/include; pwd):${C_INCLUDE_PATH:-}
+export LIBRARY_PATH=$(cd libyaml/src/.libs; pwd):${LIBRARY_PATH:-}
+export LD_LIBRARY_PATH=$(cd libyaml/src/.libs; pwd):${LD_LIBRARY_PATH:-}
+
+export PYYAML_FORCE_CYTHON=1
+export PYYAML_FORCE_LIBYAML=1
+
+# tweak CIBW behavior to run our tests for us
+export CIBW_BEFORE_BUILD='pip install Cython && make testall PYTHON=python'
+export CIBW_TEST_COMMAND='python {project}/packaging/build/smoketest.py'
+
+${PYBIN} -m cibuildwheel --platform macos .
+
 mkdir -p dist
 mv wheelhouse/* dist/
-ls -1 dist/
 
-#PYYAML_FORCE_CYTHON=1 PYYAML_FORCE_LIBYAML=1 CIBW_SKIP='pp* cp35*' CIBW_BUILD_VERBOSITY=1 CIBW_BEFORE_BUILD='pip install cython'
+ls -1 dist/
